62c39649fc9d36b8aa23b05ef16046046b967e80
Lix Paulian <lix@paulian.net>
2015-11-08 17:03:16 +0100

    Added scripts for stm32f4xx CMSIS and HAL xpacks

---
+++ add-stm32f4-cmsis.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+if (( $# < 2 ))
+then
+    echo "usage: add-cmsis.sh project_root_dir controller_type"
+    echo "example: bash add-stm32f4-cmsis.sh ~user/Projects/CortexM4 stm32f427xx"
+    exit 1
+fi
+
+# project location
+PROJECT_LOCATION=$1 # e.g. $HOME/Projects/ARM-CortexMx/CortexM4
+DEST_LOCATION="${PROJECT_LOCATION}/xpacks/cmsis"
+DEST_INCLUDE_DIR="${DEST_LOCATION}/include"
+DEST_SOURCE_DIR="${DEST_LOCATION}/src"
+PROJECT_CONFIG_DIR="${PROJECT_LOCATION}/include"
+
+# package definitions
+PACK_LOCATION="$HOME/Projects/xpacks/stm32f4-cmsis.git"
+PACK_INCLUDE_DIR="${PACK_LOCATION}/Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+PACK_SOURCE_DIR="${PACK_LOCATION}/Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates"
+PACK_GIT_HOME="https://github.com/xpacks/stm32f4-cmsis.git"
+
+if [ ! -e ${PACK_LOCATION} ]
+then
+    mkdir -p ${PACK_LOCATION}
+    git clone $PACK_GIT_HOME ${PACK_LOCATION}
+fi
+
+# package specific definitions
+CMSIS_CORE_PACK_LOCATION="$HOME/Projects/xpacks/arm-cmsis-core.git"
+CMSIS_CORE_PACK_GIT_HOME="https://github.com/xpacks/arm-cmsis-core.git"
+
+if [ ! -e ${CMSIS_CORE_PACK_LOCATION} ]
+then
+    mkdir -p ${CMSIS_CORE_PACK_LOCATION}
+    git clone $CMSIS_CORE_PACK_GIT_HOME ${CMSIS_CORE_PACK_LOCATION}
+fi
+
+# delete old components
+rm -rf "${DEST_LOCATION}"
+
+# create new dirs
+mkdir -p "${DEST_INCLUDE_DIR}"
+mkdir -p "${DEST_SOURCE_DIR}"
+
+#copy required files
+cp "${PACK_INCLUDE_DIR}"/cmsis_device.h "${DEST_INCLUDE_DIR}"
+cp "${PACK_INCLUDE_DIR}"/stm32f4xx.h "${DEST_INCLUDE_DIR}"
+cp "${PACK_INCLUDE_DIR}"/system_stm32f4xx.h "${DEST_INCLUDE_DIR}"
+cp "${PACK_INCLUDE_DIR}"/$2.h "${DEST_INCLUDE_DIR}"
+cp "${PACK_SOURCE_DIR}"/system_stm32f4xx.c "${DEST_SOURCE_DIR}"
+cp "${PACK_SOURCE_DIR}"/vectors_$2.c "${DEST_SOURCE_DIR}"
+cp "${CMSIS_CORE_PACK_LOCATION}/CMSIS/Include"/*.h "${DEST_INCLUDE_DIR}"
+
---
+++ add-stm32f4-hal.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+if (( $# < 1 ))
+then
+    echo "usage: add-stm32f4-hal.sh project_root_dir"
+    echo "example: bash add-stm32f4-hal.sh ~user/Projects/CortexM4"
+    exit 1
+fi
+
+# project location and definitions
+PROJECT_LOCATION=$1
+DEST_LOCATION="${PROJECT_LOCATION}/xpacks/stm32f4-hal"
+DEST_INCLUDE_DIR="${DEST_LOCATION}/include"
+DEST_SOURCE_DIR="${DEST_LOCATION}/src"
+PROJECT_CONFIG_DIR="${PROJECT_LOCATION}/include"
+
+# package definitions
+PACK_LOCATION="$HOME/Projects/xpacks/stm32f4-hal.git"
+PACK_INCLUDE_DIR="${PACK_LOCATION}/Drivers/STM32F4xx_HAL_Driver/Inc"
+PACK_SOURCE_DIR="${PACK_LOCATION}/Drivers/STM32F4xx_HAL_Driver/Src"
+PACK_GIT_HOME="https://github.com/xpacks/stm32f4-hal.git"
+
+if [ ! -e ${PACK_LOCATION} ]
+then
+    mkdir -p ${PACK_LOCATION}
+    git clone $PACK_GIT_HOME ${PACK_LOCATION}
+fi
+
+# delete old components
+rm -rf "${DEST_LOCATION}"
+
+# create new dirs
+mkdir -p "${DEST_INCLUDE_DIR}"
+mkdir -p "${DEST_INCLUDE_DIR}/Legacy"
+mkdir -p "${DEST_SOURCE_DIR}"
+
+# copy required files
+cp "${PACK_INCLUDE_DIR}"/*.h "${DEST_INCLUDE_DIR}"
+cp "${PACK_INCLUDE_DIR}/Legacy"/*.h "${DEST_INCLUDE_DIR}/Legacy"
+cp "${PACK_SOURCE_DIR}"/*.c "${DEST_SOURCE_DIR}"
+
+# delete the template file copied in include dir
+rm "${DEST_INCLUDE_DIR}"/stm32f4xx_hal_conf_template.h
+
+# copy configuration file
+if [ ! -e "${PROJECT_CONFIG_DIR}"/stm32f4xx_hal_conf.h ]
+then
+    cp "${PACK_INCLUDE_DIR}"/stm32f4xx_hal_conf_template.h "${PROJECT_CONFIG_DIR}"/stm32f4xx_hal_conf.h
+fi
+
