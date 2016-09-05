2c6a93bfd6544ed4f811f39ab19ad5735cf47c0d
Lix N. Paulian <NPaulian@arri.at>
2016-09-05 17:04:33 +0200

    Gave up drivers and BSP code
    Don't copy the drivers (emdrv) and BSP (bsp) files anymore, as we don’t
    use them anymore.

--- add-efr32fg-rail.sh
+++ add-efr32fg-rail.sh
@@ -43,32 +43,32 @@ rm -rf "${PROJECT_LOCATION}/xpacks/efr32fg"
 # create new dirs
 #mkdir -p "${DEST_INCLUDE_DIR}/${CMSIS_DIR}"
 mkdir -p "${DEST_INCLUDE_DIR}/${DEVICE_DIR}"
-mkdir -p "${DEST_INCLUDE_DIR}/${DRIVERS_DIR}"
+#mkdir -p "${DEST_INCLUDE_DIR}/${DRIVERS_DIR}"
 mkdir -p "${DEST_INCLUDE_DIR}/${LIBRARY_DIR}"
-mkdir -p "${DEST_INCLUDE_DIR}/${BSP_DIR}"
+#mkdir -p "${DEST_INCLUDE_DIR}/${BSP_DIR}"
 mkdir -p "${DEST_INCLUDE_DIR}/${RAIL_LIB}"
 
 mkdir -p "${DEST_SOURCE_DIR}/${DEVICE_DIR}"
-mkdir -p "${DEST_SOURCE_DIR}/${DRIVERS_DIR}"
+#mkdir -p "${DEST_SOURCE_DIR}/${DRIVERS_DIR}"
 mkdir -p "${DEST_SOURCE_DIR}/${LIBRARY_DIR}"
-mkdir -p "${DEST_SOURCE_DIR}/${BSP_DIR}"
+#mkdir -p "${DEST_SOURCE_DIR}/${BSP_DIR}"
 mkdir -p "${DEST_SOURCE_DIR}/${RAIL_LIB}"
 
 # copy required files
 #cp "${PACK_LOCATION}/submodules/${CMSIS_DIR}/Include"/*.h "${DEST_INCLUDE_DIR}/${CMSIS_DIR}"
 cp "${PACK_LOCATION}/submodules/Device/SiliconLabs/${DEVICE_DIR}/Include"/*.h "${DEST_INCLUDE_DIR}/${DEVICE_DIR}"
 cp "${PACK_LOCATION}/submodules/${LIBRARY_DIR}/inc"/*.h "${DEST_INCLUDE_DIR}/${LIBRARY_DIR}"
-find "${PACK_LOCATION}/submodules/${DRIVERS_DIR}" -name '*.h' -exec cp '{}' "${DEST_INCLUDE_DIR}/${DRIVERS_DIR}" \;
-cp "${PACK_LOCATION}/submodules/kits/common/${BSP_DIR}"/*.h "${DEST_INCLUDE_DIR}/${BSP_DIR}"
-cp "${PACK_LOCATION}/submodules/kits/EFR32FG1_BRD4252A/config"/*.h "${DEST_INCLUDE_DIR}/${BSP_DIR}"
+# find "${PACK_LOCATION}/submodules/${DRIVERS_DIR}" -name '*.h' -exec cp '{}' "${DEST_INCLUDE_DIR}/${DRIVERS_DIR}" \;
+# cp "${PACK_LOCATION}/submodules/kits/common/${BSP_DIR}"/*.h "${DEST_INCLUDE_DIR}/${BSP_DIR}"
+# cp "${PACK_LOCATION}/submodules/kits/EFR32FG1_BRD4252A/config"/*.h "${DEST_INCLUDE_DIR}/${BSP_DIR}"
 cp "${PACK_LOCATION}"/submodules/rail_lib/common/*.h "${DEST_INCLUDE_DIR}/${RAIL_LIB}"
 cp "${PACK_LOCATION}"/submodules/rail_lib/chip/efr32/*.h "${DEST_INCLUDE_DIR}/${RAIL_LIB}"
 cp "${PACK_LOCATION}"/submodules/rail_lib/chip/efr32/rf/common/cortex/*.h "${DEST_INCLUDE_DIR}/${RAIL_LIB}"
 
 cp "${PACK_LOCATION}/submodules/Device/SiliconLabs/${DEVICE_DIR}/Source"/*.c "${DEST_SOURCE_DIR}/${DEVICE_DIR}"
 cp "${PACK_LOCATION}/submodules/${LIBRARY_DIR}/src"/*.c "${DEST_SOURCE_DIR}/${LIBRARY_DIR}"
-find "${PACK_LOCATION}/submodules/${DRIVERS_DIR}" -name '*.c' -exec cp '{}' "${DEST_SOURCE_DIR}/${DRIVERS_DIR}" \;
-cp "${PACK_LOCATION}/submodules/kits/common/${BSP_DIR}"/*.c "${DEST_SOURCE_DIR}/${BSP_DIR}"
+# find "${PACK_LOCATION}/submodules/${DRIVERS_DIR}" -name '*.c' -exec cp '{}' "${DEST_SOURCE_DIR}/${DRIVERS_DIR}" \;
+# cp "${PACK_LOCATION}/submodules/kits/common/${BSP_DIR}"/*.c "${DEST_SOURCE_DIR}/${BSP_DIR}"
 cp "${PACK_LOCATION}"/lib/librail_efr32.a "${DEST_SOURCE_DIR}/${RAIL_LIB}"
 
 
