d68910fbca893748c46ceea42f6238542363058c
Lix N. Paulian <NPaulian@arri.at>
2017-03-31 16:51:58 +0200

    Renamed the script; supports now the whole Geko family of radio S0Cs

--- add-efr32xx-rail.sh
+++ add-efr32xx-rail.sh
@@ -5,22 +5,22 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if (( $# < 1 ))
+if (( $# < 2 ))
 then
-    echo "usage: add-efr32fg-rail.sh project_root_dir"
-    echo "example: bash add-efr32fg-rail.sh"
+    echo "usage: add-efr32xx-rail.sh project_root_dir"
+    echo "example: bash add-efr32xx-rail.sh"
     exit 1
 fi
 
 # project location and definitions
-PROJECT_LOCATION=$1
+PROJECT_LOCATION=$2
 
 # main destination directories
-DEST_INCLUDE_DIR="${PROJECT_LOCATION}/xpacks/efr32fg/include"
-DEST_SOURCE_DIR="${PROJECT_LOCATION}/xpacks/efr32fg/src"
+DEST_INCLUDE_DIR="${PROJECT_LOCATION}/xpacks/efr32xg/include"
+DEST_SOURCE_DIR="${PROJECT_LOCATION}/xpacks/efr32xg/src"
 
-# sub-dirs to copy files to
-DEVICE_DIR="EFR32FG1P"
+# sub-dirs to copy files from/to
+DEVICE_DIR=$1
 LIBRARY_DIR="emlib"
 BSP_DIR="bsp"
 RAIL_LIB="rail-lib"
@@ -36,26 +36,26 @@ then
 fi
 
 # delete old components
-rm -rf "${PROJECT_LOCATION}/xpacks/efr32fg"
+rm -rf "${PROJECT_LOCATION}/xpacks/efr32xg"
 
 # create new dirs
-mkdir -p "${DEST_INCLUDE_DIR}/${DEVICE_DIR}"
+mkdir -p "${DEST_INCLUDE_DIR}/efr32xg"
 mkdir -p "${DEST_INCLUDE_DIR}/${LIBRARY_DIR}"
 mkdir -p "${DEST_INCLUDE_DIR}/${RAIL_LIB}"
 
-mkdir -p "${DEST_SOURCE_DIR}/${DEVICE_DIR}"
+mkdir -p "${DEST_SOURCE_DIR}/efr32xg"
 mkdir -p "${DEST_SOURCE_DIR}/${LIBRARY_DIR}"
 mkdir -p "${DEST_SOURCE_DIR}/${RAIL_LIB}"
 
 # copy required files
-cp "${PACK_LOCATION}/platform/Device/SiliconLabs/${DEVICE_DIR}/Include"/*.h "${DEST_INCLUDE_DIR}/${DEVICE_DIR}"
+cp "${PACK_LOCATION}/platform/Device/SiliconLabs/${DEVICE_DIR}/Include"/*.h "${DEST_INCLUDE_DIR}/efr32xg"
 cp "${PACK_LOCATION}/platform/${LIBRARY_DIR}/inc"/*.h "${DEST_INCLUDE_DIR}/${LIBRARY_DIR}"
 cp "${PACK_LOCATION}/platform/radio/rail_lib/common"/*.h "${DEST_INCLUDE_DIR}/${RAIL_LIB}"
 cp "${PACK_LOCATION}/platform/radio/rail_lib/chip/efr32"/*.h "${DEST_INCLUDE_DIR}/${RAIL_LIB}"
 cp "${PACK_LOCATION}/platform/radio/rail_lib/chip/efr32/rf/common/cortex"/*.h "${DEST_INCLUDE_DIR}/${RAIL_LIB}"
 find "${PACK_LOCATION}/platform/radio/rail_lib/protocol" -name '*.h' -exec cp '{}' "${DEST_INCLUDE_DIR}/${RAIL_LIB}" \;
 
-cp "${PACK_LOCATION}/platform/Device/SiliconLabs/${DEVICE_DIR}/Source"/*.c "${DEST_SOURCE_DIR}/${DEVICE_DIR}"
+cp "${PACK_LOCATION}/platform/Device/SiliconLabs/${DEVICE_DIR}/Source"/*.c "${DEST_SOURCE_DIR}/efr32xg"
 cp "${PACK_LOCATION}/platform/${LIBRARY_DIR}/src"/*.c "${DEST_SOURCE_DIR}/${LIBRARY_DIR}"
 cp "${PACK_LOCATION}"/lib/librail_efr32.a "${DEST_SOURCE_DIR}/${RAIL_LIB}"
 
